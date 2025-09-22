f90f659a9950c52f3c45a5c437f954aa594146fb
bipin bisht <bipinbisht804@gmail.com>
2025-09-22 13:57:26 +0530

    kafka added topic on user service during user registeration

---
+++ src/main/java/com/bipin/UserService/dto/EmailDto.java
@@ -0,0 +1,41 @@
+package com.bipin.UserService.dto;
+
+public class EmailDto {
+	String to;
+	String from;
+	String subject;
+	String body;
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+}
--- src/main/java/com/bipin/UserService/services/UserService.java
+++ src/main/java/com/bipin/UserService/services/UserService.java
@@ -5,13 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bipin.UserService.client.KafkaProducerClient;
 import com.bipin.UserService.config.BcryptEncoder;
+import com.bipin.UserService.dto.EmailDto;
 import com.bipin.UserService.dto.UserLoginDto;
 import com.bipin.UserService.exception.PasswordNotFoundException;
 import com.bipin.UserService.exception.UserNotFoundException;
 import com.bipin.UserService.model.User;
 import com.bipin.UserService.repo.UserRepo;
 import com.bipin.UserService.security.JwtService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class UserService {
@@ -25,12 +29,31 @@ public class UserService {
 	@Autowired
 	private BcryptEncoder bcryptEncoder;
 
+	@Autowired
+	KafkaProducerClient kafkaProducerClient;
+
+	@Autowired
+	ObjectMapper objectMapper;
+
 	public String save(User user) {
 		String rawPassword = user.getPassword();
 		String encodedPassword = bcryptEncoder.encode(rawPassword);
 		user.setPassword(encodedPassword);
 		User savedUser = userRepo.save(user);
 		String token = jwtService.generateToken(user.getEmail());
+
+		try {
+			// Send welcome email to the user
+			EmailDto emailDto = new EmailDto();
+			emailDto.setTo(user.getEmail());
+			emailDto.setFrom("anuragonhiring@gmail.com");
+			emailDto.setSubject("Welcome Onboard!!");
+			emailDto.setBody("Your account has been created successfully!!");
+			String message = objectMapper.writeValueAsString(emailDto);
+			kafkaProducerClient.sendMessage("signup", message);
+		} catch (JsonProcessingException exception) {
+			throw new RuntimeException(exception.getMessage());
+		}
 		return token;
 	}
 
